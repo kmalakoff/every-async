124ea19f95e7dd5e9415ff248e95f17249e46014
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-19 21:01:47 -0700

    Simplify implementation

--- index.js
+++ index.js
@@ -1,60 +1,76 @@
+function call0(next, fn) {
+  fn(next);
+}
+
+function call1(arg1, next, fn) {
+  fn(arg1, next);
+}
+
+function call2(arg1, arg2, next, fn) {
+  fn(arg1, arg2, next);
+}
+
+function call3(arg1, arg2, arg3, next, fn) {
+  fn(arg1, arg2, arg3, next);
+}
+
+function call4(arg1, arg2, arg3, arg4, next, fn) {
+  fn(arg1, arg2, arg3, arg4, next);
+}
+
+function call5(arg1, arg2, arg3, arg4, arg5, next, fn) {
+  fn(arg1, arg2, arg3, arg4, arg5, next);
+}
+
+function call6(arg1, arg2, arg3, arg4, arg5, arg6, next, fn) {
+  fn(arg1, arg2, arg3, arg4, arg5, arg6, next);
+}
+
+function calln(args, fn) {
+  fn.apply(null, args);
+}
+
 module.exports = function every(fns, arg1, arg2, arg3, arg4, arg5, arg6, callback) {
   var call;
   switch (arguments.length) {
     case 2:
-      call = function call0() {
-        fns[index](next);
-      };
+      call = call0.bind(null, next);
       callback = arg1;
       break;
     case 3:
-      call = function call1() {
-        fns[index](arg1, next);
-      };
+      call = call1.bind(null, arg1, next);
       callback = arg2;
       break;
     case 4:
-      call = function call2() {
-        fns[index](arg1, arg2, next);
-      };
+      call = call2.bind(null, arg1, arg2, next);
       callback = arg3;
       break;
     case 5:
-      call = function call3() {
-        fns[index](arg1, arg2, arg3, next);
-      };
+      call = call3.bind(null, arg1, arg2, arg3, next);
       callback = arg4;
       break;
     case 6:
-      call = function call4() {
-        fns[index](arg1, arg2, arg3, arg4, next);
-      };
+      call = call4.bind(null, arg1, arg2, arg3, arg4, next);
       callback = arg5;
       break;
     case 7:
-      call = function call5() {
-        fns[index](arg1, arg2, arg3, arg4, arg5, next);
-      };
+      call = call5.bind(null, arg1, arg2, arg3, arg4, arg5, next);
       callback = arg6;
       break;
     case 8:
-      call = function call6() {
-        fns[index](arg1, arg2, arg3, arg4, arg5, arg6, next);
-      };
+      call = call6.bind(null, arg1, arg2, arg3, arg4, arg5, arg6, next);
       break;
     default:
       var args = Array.prototype.slice.call(arguments, 1);
       callback = args.pop();
       args.push(next);
-      call = function calln() {
-        fns[index].apply(null, args);
-      };
+      call = calln.bind(null, args);
   }
 
   var index = -1;
   function next(err, result) {
     if (err || !result || ++index >= fns.length) return callback(err, result);
-    call();
+    call(fns[index]);
   }
   next(null, true);
 };
