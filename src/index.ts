4578a70b453310cd5a9f90afa3fdfef2f6905b42
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-19 00:36:13 -0800

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export type Callback = (error?: Error, value?: unknown) => void;
 export type CallFn = (cb: Callback) => void;
 
-export default function every<T extends unknown[]>(fns: Callback[], ...args: [...T, Callback]): undefined {
+export default function every<T extends unknown[]>(fns: Callback[], ...args: [...T, Callback]): void {
   const callback = args[args.length - 1] as Callback;
   args[args.length - 1] = next; // replace callback with next
   let index = -1;
