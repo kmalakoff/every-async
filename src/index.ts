66162a7b4ecdaa85250035d0b261af972fd0c4a3
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 14:23:45 -0700

    Fix broken types #2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export type Callback = (error?: Error, value?: unknown) => void;
+export type Callback = (error?: Error | null, value?: unknown) => void;
 export type CallFn = (cb: Callback) => void;
 
 export default function every<T extends unknown[]>(fns: Callback[], ...args: [...T, Callback]): void {
   const callback = args[args.length - 1] as Callback;
   args[args.length - 1] = next; // replace callback with next
   let index = -1;
-  function next(err: Error | undefined, result?: unknown) {
+  function next(err?: Error | null, result?: unknown) {
     if (err || !result || ++index >= fns.length) return callback(err, result);
     fns[index].apply(null, args as unknown as Parameters<Callback>);
   }
